4b9297bb176d84448d49d7a992ea8243e3898c38
Steven Cocks <ext.steven.cocks@king.com>
2024-03-15 08:41:31 +0100

    first commit

---
+++ src/main/java/org/libDep5/Library.java
@@ -0,0 +1,13 @@
+package org.libDep5;
+public class Library {
+    public String[] count(Integer number) {
+        String[] lines = new String[number];
+        for (int i = 0; i < number; i++) {
+            lines[i] = "i = " + i;
+        }
+        return lines;
+    }
+    public String greet(String name) {
+        return "Kon'nichiwa, " + name + "!";
+    }
+}
---
+++ src/test/java/org/libDep5/MainTest.java
@@ -0,0 +1,24 @@
+package org.libDep5;
+
+import org.junit.jupiter.api.Test;
+import org.libDep5.Library;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void testCount() {
+        Library library = new Library();
+        String[] result = library.count(10);
+        assertEquals(10, result.length);
+        for (int i = 0; i < 10; i++) {
+            assertEquals("i = " + i, result[i]);
+        }
+    }
+    @Test
+    void testGreet() {
+        Library library = new Library();
+        String result = library.greet("lib-dep-5");
+        assertEquals("Kon'nichiwa, lib-dep-5!", result);
+    }
+}
